8f79025a57ad23911492af5c8589a3202150a041
Gleb Novichkov, PhD <gleb.novitchkov@gmail.com>
2023-04-28 21:49:36 +0300

    Cleaning

--- src/freq.cpp
+++ src/freq.cpp
@@ -32,7 +32,7 @@ std::vector<std::string> tokenize (const std::string& input, const std::string& 
 }
 
 void print_usage(char* self) {
-	std::cout << "Usage: " << self << " <input_file.txt> " << std::endl;
+	std::cout << "Usage: " << self << " <input_file.txt> <output_file.txt>" << std::endl;
 }
 
 int main(int argc, char** argv) try {
@@ -50,19 +50,24 @@ int main(int argc, char** argv) try {
     };
     std::set <std::pair<std::string, size_t>, decltype(comparator)> 	freq_orderer(comparator);
 
-	if (argc<2) {
+	if (argc<3) {
 		print_usage(argv[0]);
 		std::exit(EXIT_SUCCESS);
 	}
 
     std::ifstream  input_fs(argv[1]);
+    std::ofstream  output_fs(argv[2]);
 
     if (input_fs.fail()) {
         throw std::runtime_error ("***ERROR*** Could not read file `"+std::string(argv[1])+"'. Aborting.");
     }
 
-    //	Reading file line-by-line, and store tokens into a single 
-    //	(possibly large) vector of tokens
+    if (output_fs.fail()) {
+        throw std::runtime_error ("***ERROR*** Could not open file `"+std::string(argv[1])+"' for writing. Aborting.");
+    }
+
+    //	Reading file line-by-line, and store tokens into a 
+    //	single (possibly large) vector of tokens
     std::vector<std::string>	words;
 
     for(std::string line; getline(input_fs, line); ) {
@@ -82,7 +87,7 @@ int main(int argc, char** argv) try {
 
     //	Saving to a file
     for (auto & [word, count]: freq_orderer) {
-    	std::cout << count << "\t" << word << std::endl;
+    	output_fs << count << " " << word << std::endl;
     }
 } 
 catch (std::runtime_error& e) {
